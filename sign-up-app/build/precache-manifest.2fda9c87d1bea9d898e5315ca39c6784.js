129e170dcf5f06396bf0cd0ded0c62481b5bbebd
marina budz <budz.marina97@gmail.com>
2020-07-26 02:01:35 +0300

    change
    change

---
+++ sign-up-app/build/precache-manifest.2fda9c87d1bea9d898e5315ca39c6784.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d417707cf6807702de2c",
+    "url": "/sign-up-app/static/css/main.4ae4efda.chunk.css"
+  },
+  {
+    "revision": "d417707cf6807702de2c",
+    "url": "/sign-up-app/static/js/main.0f4a67cd.chunk.js"
+  },
+  {
+    "revision": "62a60c07f3c94f3473c6",
+    "url": "/sign-up-app/static/js/runtime~main.a310f9af.js"
+  },
+  {
+    "revision": "3f03175b3c89cae4abd6",
+    "url": "/sign-up-app/static/js/2.b74005f8.chunk.js"
+  },
+  {
+    "revision": "ea276202266b3b13cd4dc5b9ab23d726",
+    "url": "/sign-up-app/index.html"
+  }
+];
